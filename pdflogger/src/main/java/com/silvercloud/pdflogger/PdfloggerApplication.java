c316eff918a3f6949ef47a424e609c3ddf67e073
SnehaMane17 <snehamane171998@gmail.com>
2022-06-18 15:05:53 +0530

    Second commit

---
+++ pdflogger/src/main/java/com/silvercloud/pdflogger/PdfloggerApplication.java
@@ -0,0 +1,13 @@
+package com.silvercloud.pdflogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfloggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfloggerApplication.class, args);
+	}
+
+}
---
+++ pdflogger/src/test/java/com/silvercloud/pdflogger/PdfloggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silvercloud.pdflogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfloggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
